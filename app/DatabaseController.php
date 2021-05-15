854963858c7a9398d12f82f58ff0e28b91c8d2f6
Diego Quiroz <diego00aq@gmail.com>
2021-05-15 15:49:36 -0500

    Add Database connection using .env.example
    NOTE: maybe delete env vars.
    Use a Controller arch.

---
+++ app/DatabaseController.php
@@ -0,0 +1,43 @@
+<?php
+
+class DatabaseController {
+
+    private $connection;
+
+    private $host;
+    private $port;
+    private $database;
+    private $user;
+    private $password;
+
+    /*
+    * An instance of this class will connect to our
+    * database using data from .env
+    */
+    public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->port = getenv('DB_PORT');
+        $this->database = getenv('DB_DATABASE');
+        $this->user = getenv('DB_USER');
+        $this->password = getenv('DB_PASSWORD');
+        
+        $this->connection = new PDO (
+            "mysql:host=$this->host;port=$this->port;charset=utf8mb4;dbname=$this->database",
+            $this->user,
+            $this->password
+        );
+        
+    }
+
+    // Returns PDO connection
+    public function getConnection() { return $this->connection; }
+
+    // Returns host name of db
+    public function getHost() { return $this->host; }
+    // Returns port of mysql
+    public function getPort() { return $this->port; }
+    // Returns database name
+    public function getDatabase() { return $this->database; }
+    // Returns username of mysql
+    public function getUser() { return $this->user; }
+}
